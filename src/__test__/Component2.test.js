115bece6e2d1d18f38eb116a48c7711a5f5ef92d
peterlidee <peter@lidee.be>
2022-08-21 18:40:23 +0200

    Added component 2 + test

---
+++ src/__test__/Component2.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Component2 from '../components/Component2'
+
+test('Component2 renders', () => {
+  render(<Component2 />)
+  
+  // method 1
+  expect(screen.getByRole('button', { name: 'add' })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: 'subtract' })).toBeInTheDocument()
+
+  // method 2
+  const buttons = screen.getAllByRole('button')
+  expect(buttons[0]).toBeInTheDocument()
+  expect(buttons[1]).toBeInTheDocument()
+})
---
+++ src/components/Component2.js
@@ -0,0 +1,9 @@
+function Component2(){
+  return(
+    <div className="Component2">
+      <button>add</button>
+      <button>subtract</button>
+    </div>
+  )
+}
+export default Component2
