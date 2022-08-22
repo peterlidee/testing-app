c89e2de7344185ba700ea382b4c20f7a0622a0ea
peterlidee <peter@lidee.be>
2022-08-22 18:39:43 +0200

    Flipped add and subtract

--- src/__tests__/Component2.test.js
+++ src/__tests__/Component2.test.js
@@ -5,8 +5,8 @@ test('Component2 renders', () => {
   render(<Component2 />)
   
   // method 1
-  expect(screen.getByRole('button', { name: 'add' })).toBeInTheDocument()
   expect(screen.getByRole('button', { name: 'subtract' })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: 'add' })).toBeInTheDocument()
 
   // method 2
   const buttons = screen.getAllByRole('button')
--- src/components/Component2.js
+++ src/components/Component2.js
@@ -2,8 +2,8 @@ function Component2(){
   return(
     <div className="Component2">
       <h4>Component 2</h4>
-      <button>add</button>
       <button>subtract</button>
+      <button>add</button>
     </div>
   )
 }
